451f954ff6d45914cbe7e7e7b303657bb0eb0584
Dima Romanenko <79811705+iPanda177@users.noreply.github.com>
2023-11-14 13:47:54 +0200

    api version fix

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -13,7 +13,7 @@ import prisma from "./db.server";
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.January23,
+  apiVersion: ApiVersion.July23,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
