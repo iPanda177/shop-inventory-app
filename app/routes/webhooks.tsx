8fd286d6aa125e088e637bfc3636ea3e48c5cf18
viacheslavbadaiev <viacheslavbadaiev@devit.group>
2023-12-21 18:07:46 +0200

    fix webhooks

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,168 +1,192 @@
-import type { ActionFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
-import {json} from "@remix-run/node";
-import type {OrderProduct, Order, Product, InventoryItem, ShopifyProduct, LineItem} from "~/types";
-import {handleOrder, saveProduct, updateInventory} from "~/services/products.server";
+import type { ActionFunctionArgs } from '@remix-run/node'
+import { authenticate } from '../shopify.server'
+import { json } from '@remix-run/node'
+import type {
+  OrderProduct,
+  Order,
+  Product,
+  ShopifyProduct,
+  LineItem,
+} from '~/types'
+import type { InventoryItem } from 'node_modules/@shopify/shopify-api/rest/admin/2023-10/inventory_item'
+import {
+  handleOrder,
+  saveProduct,
+  updateInventory,
+} from '~/services/products.server'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const {
-    topic,
-    shop,
-    session,
-    admin,
-    payload } = await authenticate.webhook(request);
+  const { topic, shop, session, admin, payload } = await authenticate.webhook(
+    request,
+  )
 
   if (!admin) {
-    throw new Response();
+    throw new Response()
   }
 
   switch (topic) {
-    case "PRODUCTS_CREATE":
+    case 'PRODUCTS_CREATE':
       try {
-        const { id, title, status, image, variants } = payload as unknown as ShopifyProduct;
-        const inventory_id: string[] = [];
+        const { id, title, status, image, variants } =
+          payload as unknown as ShopifyProduct
+        const inventory_id: string[] = []
 
-        await Promise.all(variants.map(async variant => {
-          const product: Product = {
-            store: shop,
-            product_id: String(id),
-            title: title,
-            product_image: image && image.src,
-            isActive: status === 'active',
-            variant_id: String(variant.id),
-            variant_title: variant.title,
-            inventory: variant.inventory_quantity,
-            inventory_item_id: String(variant.inventory_item_id),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          }
-
-          if (variant.inventory_item_id) {
-            inventory_id.push(String(variant.inventory_item_id))
-          }
-
-          await saveProduct(product)
-        }));
-
-        const inventory: { inventory_items: InventoryItem[] } = await admin.rest.resources.InventoryItem.all({
-          session: session,
-          ids: inventory_id.toString(),
-        });
-
-        await Promise.all(inventory.inventory_items.map(async inventory_data => {
-          await updateInventory(inventory_data);
-          return true
-        }))
-
-        return json({message: "OK"}, {status: 200});
-      } catch (error) {
-        return json({message: error}, {status: 500})
-      }
-
-    case "PRODUCTS_UPDATE":
-      try {
-        const { id, title, status, image, variants } = payload as unknown as ShopifyProduct;
-        let inventory_id: string[] = [];
-
-        await Promise.all(variants.map(async variant => {
-          const product: Product = {
-            store: shop,
-            product_id: String(id),
-            title: title,
-            product_image: image && image.src,
-            isActive: status === 'active',
-            variant_id: String(variant.id),
-            variant_title: variant.title,
-            inventory: variant.inventory_quantity,
-            inventory_item_id: String(variant.inventory_item_id),
-            updatedAt: new Date(),
-          }
-
-          inventory_id.push(String(variant.inventory_item_id));
-
-          await saveProduct(product)
-        }));
-
-        const inventory: { inventory_items: InventoryItem[] } = await admin.rest.resources.InventoryItem.all({
-          session: session,
-          ids: inventory_id.toString(),
-        });
-
-        await Promise.all(inventory.inventory_items.map(async inventory_data => {
-          await updateInventory(inventory_data)
-          return true
-        }))
-
-        return json({message: "OK"}, {status: 200});
-      } catch (error) {
-        console.log("error", error)
-        return json({error: error}, {status: 500});
-      }
-
-    case "PRODUCTS_DELETE":
-      try {
-        const { id } = payload as unknown as ShopifyProduct;
-        const product = await prisma.productList.findFirst({
-            where: {
-                product_id: String(id)
+        await Promise.all(
+          variants.map(async variant => {
+            const product: Product = {
+              store: shop,
+              product_id: String(id),
+              title: title,
+              product_image: image && image.src,
+              isActive: status === 'active',
+              variant_id: String(variant.id),
+              variant_title: variant.title,
+              inventory: variant.inventory_quantity,
+              inventory_item_id: String(variant.inventory_item_id),
+              createdAt: new Date(),
+              updatedAt: new Date(),
             }
+
+            if (variant.inventory_item_id) {
+              inventory_id.push(String(variant.inventory_item_id))
+            }
+
+            await saveProduct(product)
+          }),
+        )
+
+        const inventory = await admin.rest.InventoryItem.all({
+          session: session,
+          ids: inventory_id.toString(),
+        })
+
+        await Promise.all(
+          inventory.data.map(async inventory_data => {
+            await updateInventory(inventory_data)
+            return true
+          }),
+        )
+
+        return json({ message: 'OK' }, { status: 200 })
+      } catch (error) {
+        console.log('error PRODUCTS_CREATE', error)
+        return json({ message: error }, { status: 500 })
+      }
+
+    case 'PRODUCTS_UPDATE':
+      try {
+        const { id, title, status, image, variants } =
+          payload as unknown as ShopifyProduct
+        let inventory_id: string[] = []
+
+        await Promise.all(
+          variants.map(async variant => {
+            const product: Product = {
+              store: shop,
+              product_id: String(id),
+              title: title,
+              product_image: image && image.src,
+              isActive: status === 'active',
+              variant_id: String(variant.id),
+              variant_title: variant.title,
+              inventory: variant.inventory_quantity,
+              inventory_item_id: String(variant.inventory_item_id),
+              updatedAt: new Date(),
+            }
+
+            inventory_id.push(String(variant.inventory_item_id))
+
+            await saveProduct(product)
+          }),
+        )
+
+        const inventory = await admin.rest.InventoryItem.all({
+          session: session,
+          ids: inventory_id.toString(),
+        })
+
+        await Promise.all(
+          inventory.data.map(async inventory_data => {
+            await updateInventory(inventory_data)
+            return true
+          }),
+        )
+
+        return json({ message: 'OK' }, { status: 200 })
+      } catch (error) {
+        console.log('error PRODUCTS_UPDATE', error)
+        return json({ error: error }, { status: 500 })
+      }
+
+    case 'PRODUCTS_DELETE':
+      try {
+        const { id } = payload as unknown as ShopifyProduct
+        const product = await prisma.productList.findFirst({
+          where: {
+            product_id: String(id),
+          },
         })
 
         if (!product) {
-          return json({message: "OK"}, {status: 200});
+          return json({ message: 'OK' }, { status: 200 })
         }
 
         await prisma.productList.update({
           where: {
-            id: product.id
+            id: product.id,
           },
           data: {
             isActive: false,
             updatedAt: new Date(),
-          }
+          },
         })
-        return json({message: "OK"}, {status: 200});
+        return json({ message: 'OK' }, { status: 200 })
       } catch (error) {
-        return json({message: error}, {status: 500});
+        console.log('error PRODUCTS_DELETE', error)
+        return json({ message: error }, { status: 500 })
       }
 
-    case "ORDERS_CREATE":
+    case 'ORDERS_CREATE':
       try {
-        const { line_items } = payload as unknown as Order;
+        const { line_items } = payload as unknown as Order
 
-        await Promise.all(line_items.map(async (item: LineItem) => {
-          const product: OrderProduct = {
-            store: shop,
-            product_id: String(item.product_id),
-            title: item.title,
-            buy: item.quantity,
-            variant_id: String(item.variant_id),
-            variant_title: String(item.variant_title),
-            updatedAt: new Date(),
-          }
+        await Promise.all(
+          line_items.map(async (item: LineItem) => {
+            const product: OrderProduct = {
+              store: shop,
+              product_id: String(item.product_id),
+              title: item.title,
+              buy: item.quantity,
+              variant_id: String(item.variant_id),
+              variant_title: String(item.variant_title),
+              updatedAt: new Date(),
+            }
 
-          await handleOrder(product)
-        }))
-        return json({message: "OK"}, {status: 200});
+            await handleOrder(product)
+          }),
+        )
+        return json({ message: 'OK' }, { status: 200 })
       } catch (error) {
-        return json({message: error}, {status: 500});
+        console.log('error ORDERS_CREATE', error)
+        return json({ message: error }, { status: 500 })
       }
 
-    case "INVENTORY_ITEMS_UPDATE":
+    case 'INVENTORY_ITEMS_UPDATE':
       try {
         await updateInventory(payload as unknown as InventoryItem)
-        return json({message: "OK"}, {status: 200})
+        return json({ message: 'OK' }, { status: 200 })
       } catch (error) {
-        return json({message: error}, {status: 500})
+        console.log('error INVENTORY_ITEMS_UPDATE', error)
+        return json({ message: error }, { status: 500 })
       }
 
-    case "APP_UNINSTALLED":
-    case "CUSTOMERS_DATA_REQUEST":
-    case "CUSTOMERS_REDACT":
-    case "SHOP_REDACT":
-      return json({message: "OK"}, {status: 200});
+    case 'APP_UNINSTALLED':
+    case 'CUSTOMERS_DATA_REQUEST':
+    case 'CUSTOMERS_REDACT':
+    case 'SHOP_REDACT':
+      return json({ message: 'OK' }, { status: 200 })
 
     default:
-      throw new Response("Unhandled webhook topic", { status: 404 });
+      throw new Response('Unhandled webhook topic', { status: 404 })
   }
-};
+}
--- app/services/products.server.ts
+++ app/services/products.server.ts
@@ -1,215 +1,221 @@
-import {json} from "@remix-run/node";
-import {unauthenticated} from "~/shopify.server";
-import type {InventoryItem, OrderProduct, Product} from "~/types";
+import { json } from '@remix-run/node'
+import type { InventoryItem } from 'node_modules/@shopify/shopify-api/rest/admin/2023-10/inventory_item'
+import { unauthenticated } from '~/shopify.server'
+import type { OrderProduct, Product } from '~/types'
 
 export async function getProducts(store: string, product_ids: string[]) {
-    try {
-      const ids = product_ids.join(',')
-      const { session, admin } = await unauthenticated.admin(store);
+  try {
+    const ids = product_ids.join(',')
+    const { session, admin } = await unauthenticated.admin(store)
 
-      const products = await admin.rest.resources.Product.all({
-        session,
-        ids,
-      });
+    const products = await admin.rest.resources.Product.all({
+      session,
+      ids,
+      limit: 250,
+    })
 
-      return json({products}, {status: 200});
-    } catch (error) {
-      console.log(error)
-      return json({error: error}, {status: 500});
-    }
+    return json({ products }, { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return json({ error: error }, { status: 500 })
+  }
 }
 
 export async function saveProduct(product: Product) {
-    try {
-        let date_start = new Date();
-        date_start = new Date(date_start.setHours(0, 0, 0, 0));
-        let date_end = new Date();
-        date_end = new Date(date_end.setHours(24, 0, 0, 0));
+  try {
+    let date_start = new Date()
+    date_start.setHours(0, 0, 0, 0)
+    let date_end = new Date()
+    date_end.setHours(24, 0, 0, 0)
 
-        let product_list = await prisma.productList.findFirst({
-            where: {
-                product_id: product.product_id,
-                variant_id: product.variant_id
-            }
-        })
+    let product_list = await prisma.productList.findFirst({
+      where: {
+        product_id: product.product_id,
+        variant_id: product.variant_id,
+      },
+    })
 
-        if (!product_list) {
-            product_list = await prisma.productList.create({
-                data: {
-                    store: product.store,
-                    product_id: product.product_id,
-                    product_image: product.product_image,
-                    title: product.title,
-                    isActive: product.isActive,
-                    variant_id: product.variant_id,
-                    variant_title: product.variant_title,
-                    quantity: product.inventory,
-                    inventory_item_id: product.inventory_item_id,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                }
-            })
-        } else {
-            await prisma.productList.update({
-                where: {
-                    id: product_list.id
-                },
-                data: {
-                    store: product.store,
-                    product_id: product.product_id,
-                    product_image: product.product_image,
-                    title: product.title,
-                    isActive: product.isActive,
-                    variant_id: product.variant_id,
-                    variant_title: product.variant_title,
-                    quantity: product.inventory,
-                    inventory_item_id: product.inventory_item_id,
-                    updatedAt: new Date(),
-                },
-            });
-        }
-
-        const productData = await prisma.product.findFirst({
-            where: {
-                product_id: product.product_id,
-                variant_id: product.variant_id,
-                updatedAt: {
-                    gte: date_start,
-                    lte: date_end
-                }
-            }
-        })
-
-        if (!productData) {
-            await prisma.product.create({
-                data: {
-                    store: product.store,
-                    product_id: product.product_id,
-                    product_image: product.product_image,
-                    title: product.title,
-                    isActive: product.isActive,
-                    variant_id: product.variant_id,
-                    variant_title: product.variant_title,
-                    inventory: product.inventory,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                    is_out_stock: product.inventory <= 0 && product_list.tracked,
-                }
-            })
-        }
-    } catch (error) {
-        console.log("error", error)
+    if (!product_list) {
+      product_list = await prisma.productList.create({
+        data: {
+          store: product.store,
+          product_id: product.product_id,
+          product_image: product.product_image,
+          title: product.title,
+          isActive: product.isActive,
+          variant_id: product.variant_id,
+          variant_title: product.variant_title,
+          quantity: product.inventory,
+          inventory_item_id: product.inventory_item_id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      })
+    } else {
+      await prisma.productList.update({
+        where: {
+          id: product_list.id,
+        },
+        data: {
+          store: product.store,
+          product_id: product.product_id,
+          product_image: product.product_image,
+          title: product.title,
+          isActive: product.isActive,
+          variant_id: product.variant_id,
+          variant_title: product.variant_title,
+          quantity: product.inventory,
+          inventory_item_id: product.inventory_item_id,
+          updatedAt: new Date(),
+        },
+      })
     }
+
+    const productData = await prisma.product.findFirst({
+      where: {
+        product_id: product.product_id,
+        variant_id: product.variant_id,
+        updatedAt: {
+          gte: date_start,
+          lte: date_end,
+        },
+      },
+    })
+
+    if (!productData) {
+      await prisma.product.create({
+        data: {
+          store: product.store,
+          product_id: product.product_id,
+          product_image: product.product_image,
+          title: product.title,
+          isActive: product.isActive,
+          variant_id: product.variant_id,
+          variant_title: product.variant_title,
+          inventory: product.inventory,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          is_out_stock: product.inventory <= 0 && product_list.tracked,
+        },
+      })
+    }
+  } catch (error) {
+    console.log('error', error)
+  }
 }
 
 export async function updateInventory(inventory_item: InventoryItem) {
-    try {
-        const inventory_product = await prisma.productList.findFirst({
-           where: {
-                inventory_item_id: inventory_item.id.toString()
-           }
-        });
-
-        if (inventory_product) {
-            await prisma.productList.update({
-                where: {
-                    id: inventory_product.id
-                },
-                data: {
-                    tracked: inventory_item.tracked,
-                    updatedAt: new Date(),
-                }
-            });
-
-            const product_variant = await prisma.product.findFirst({
-                where: {
-                    variant_id: inventory_product.variant_id,
-                },
-                orderBy: {
-                    updatedAt: 'desc'
-                }
-            });
-
-            if (!product_variant || product_variant.inventory === null) {
-                return;
-            }
-
-            await prisma.product.update({
-                where: {
-                    id: product_variant.id
-                },
-                data: {
-                    is_out_stock: product_variant.inventory <= 0 && inventory_item.tracked,
-                }
-            })
-        }
-    } catch (error) {
-        console.log(error)
+  try {
+    if (!inventory_item || !inventory_item.id) {
+      return
     }
+    const inventory_product = await prisma.productList.findFirst({
+      where: {
+        inventory_item_id: inventory_item.id.toString(),
+      },
+    })
+
+    if (inventory_product) {
+      await prisma.productList.update({
+        where: {
+          id: inventory_product.id,
+        },
+        data: {
+          tracked: !!inventory_item.tracked,
+          updatedAt: new Date(),
+        },
+      })
+
+      const product_variant = await prisma.product.findFirst({
+        where: {
+          variant_id: inventory_product.variant_id,
+        },
+        orderBy: {
+          updatedAt: 'desc',
+        },
+      })
+
+      if (!product_variant || product_variant.inventory === null) {
+        return
+      }
+
+      await prisma.product.update({
+        where: {
+          id: product_variant.id,
+        },
+        data: {
+          is_out_stock:
+            product_variant.inventory <= 0 && !!inventory_item.tracked,
+        },
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export async function handleOrder(product: OrderProduct) {
-    try {
-        let date_start = new Date()
-        date_start = new Date(date_start.setHours(0, 0, 0, 0))
-        let date_end = new Date()
-        date_end = new Date(date_end.setHours(24, 0, 0, 0))
+  try {
+    let date_start = new Date()
+    date_start.setHours(0, 0, 0, 0)
+    let date_end = new Date()
+    date_end.setHours(24, 0, 0, 0)
 
-        const product_list = await prisma.productList.findFirst({
-            where: {
-                product_id: product.product_id,
-                variant_id: product.variant_id
-            }
-        });
+    const product_list = await prisma.productList.findFirst({
+      where: {
+        product_id: product.product_id,
+        variant_id: product.variant_id,
+      },
+    })
 
-        if (!product_list) {
-            await prisma.productList.create({
-                data: {
-                    store: product.store,
-                    product_id: product.product_id,
-                    title: product.title,
-                    isActive: true,
-                    variant_id: product.variant_id,
-                    variant_title: product.variant_title,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                }
-            })
-        }
-
-        const productData = await prisma.product.findFirst({
-            where: {
-                product_id: product.product_id,
-                variant_id: product.variant_id,
-                updatedAt: { gte: date_start, lte: date_end }
-            }
-        })
-
-        if (!productData) {
-            await prisma.product.create({
-                data: {
-                    store: product.store,
-                    product_id: product.product_id,
-                    title: product.title,
-                    variant_id: product.variant_id,
-                    variant_title: product.variant_title,
-                    buy: product.buy,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                }
-            });
-        } else {
-            await prisma.product.update({
-                where: {
-                    id: productData.id
-                },
-                data: {
-                    buy: productData.buy ? productData.buy + product.buy : product.buy,
-                    updatedAt: new Date(),
-                }
-            });
-        }
-    } catch (error) {
-        console.log("error", error)
+    if (!product_list) {
+      await prisma.productList.create({
+        data: {
+          store: product.store,
+          product_id: product.product_id,
+          title: product.title,
+          isActive: true,
+          variant_id: product.variant_id,
+          variant_title: product.variant_title,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      })
     }
+
+    const productData = await prisma.product.findFirst({
+      where: {
+        product_id: product.product_id,
+        variant_id: product.variant_id,
+        updatedAt: { gte: date_start, lte: date_end },
+      },
+    })
+
+    if (!productData) {
+      await prisma.product.create({
+        data: {
+          store: product.store,
+          product_id: product.product_id,
+          title: product.title,
+          variant_id: product.variant_id,
+          variant_title: product.variant_title,
+          buy: product.buy,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      })
+    } else {
+      await prisma.product.update({
+        where: {
+          id: productData.id,
+        },
+        data: {
+          buy: productData.buy ? productData.buy + product.buy : product.buy,
+          updatedAt: new Date(),
+        },
+      })
+    }
+  } catch (error) {
+    console.log('error', error)
+  }
 }
